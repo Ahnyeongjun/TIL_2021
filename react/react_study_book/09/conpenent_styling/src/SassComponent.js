063bd9cd185946b98c534dde8c6cfaa954bded61
jun <toinin@naver.com>
2021-03-03 16:49:54 +0900

    Add before npm eject

---
+++ react/react_study_book/09/conpenent_styling/src/SassComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './SassComponent.scss';
+
+const SassComponent = () => {
+    return (
+        <div className="SassComponent">
+            <div className="box red" />
+        </div>
+    )
+}
+export default SassComponent;
