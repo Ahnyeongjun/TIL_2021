e5d6696a5c8a338ebff46577cedb8f5fefa258f2
jun <toinin@naver.com>
2021-03-05 18:41:46 +0900

    Add react study book 14.5 ~ 14.6

--- react/react_study_book/13/router-tutorial/src/components/NewsList/NewsList.js
+++ react/react_study_book/13/router-tutorial/src/components/NewsList/NewsList.js
@@ -1,23 +1,41 @@
 import NewsItem from './NewsItem/NewsItem'
-import React from 'react';
-// import axios from 'axios'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
 import * as S from './styles';
-const sampleArticle = {
-    title: '제목',
-    description: '내용',
-    url: 'https://google.com',
-    urlToImage: 'https://via.placeholder.com/160',
-};
 
-const NewsList = () => {
+const NewsList = ({ category }) => {
+    const [articles, setArticles] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true);
+            try {
+                const query = category === 'all' ? '' : `&category =${category}`;
+                const response = await axios.get(`http://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=67a258e2d05745e2ab5fedb939d32aa8`
+                );
+
+                setArticles(response.data.articles);
+            } catch (e) {
+                console.log(e);
+            }
+            setLoading(false);
+        }
+        fetchData();
+    }, [category]);
+
+    if (loading) {
+        return <S.NewsListBlock>대기 중..</S.NewsListBlock>;
+    }
+    if (!articles) {
+        return null;
+    }
+
     return (
         <S.NewsListBlock>
-            <NewsItem article={sampleArticle} />
-            <NewsItem article={sampleArticle} />
-            <NewsItem article={sampleArticle} />
-            <NewsItem article={sampleArticle} />
-            <NewsItem article={sampleArticle} />
-            <NewsItem article={sampleArticle} />
+            {articles.map(article => (
+                <NewsItem key={article.url} article={article} />
+            ))}
         </S.NewsListBlock>
     );
 };
