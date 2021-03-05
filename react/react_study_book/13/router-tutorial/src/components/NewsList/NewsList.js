d9192e9610affa38da78de9310ec39565c47d4f5
jun <toinin@naver.com>
2021-03-05 20:15:45 +0900

    Add react study book 14.8

--- react/react_study_book/13/router-tutorial/src/components/NewsList/NewsList.js
+++ react/react_study_book/13/router-tutorial/src/components/NewsList/NewsList.js
@@ -2,35 +2,26 @@ import NewsItem from './NewsItem/NewsItem'
 import React, { useEffect, useState } from 'react';
 import axios from 'axios'
 import * as S from './styles';
+import usePromise from '../../lib/usePromis';
 
 const NewsList = ({ category }) => {
-    const [articles, setArticles] = useState(null);
-    const [loading, setLoading] = useState(false);
-
-    useEffect(() => {
-        const fetchData = async () => {
-            setLoading(true);
-            try {
-                const query = category === 'all' ? '' : `&category =${category}`;
-                const response = await axios.get(`http://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=67a258e2d05745e2ab5fedb939d32aa8`
-                );
-
-                setArticles(response.data.articles);
-            } catch (e) {
-                console.log(e);
-            }
-            setLoading(false);
-        }
-        fetchData();
+    const [loading, response, error] = usePromise(() => {
+        const query = category === 'all' ? '' : `&category=${category}`;
+        return axios.get(`http://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=67a258e2d05745e2ab5fedb939d32aa8`);
     }, [category]);
+
 
     if (loading) {
         return <S.NewsListBlock>대기 중..</S.NewsListBlock>;
     }
-    if (!articles) {
+    if (!response) {
         return null;
     }
+    if (error) {
+        return <S.NewsListBlock>에러 발생!</S.NewsListBlock>
+    }
 
+    const { articles } = response.data;
     return (
         <S.NewsListBlock>
             {articles.map(article => (
