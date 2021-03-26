d3d310686766e2b011b9c7c5f91747523a541e9b
jun <toinin@naver.com>
2021-03-26 14:42:32 +0900

    Add Register

--- react/react_study_book/24/big-frontend/src/App.js
+++ react/react_study_book/24/big-frontend/src/App.js
@@ -1,10 +1,4 @@
 import './App.css';
-import { Switch, Route } from "react-router-dom";
-import PostListPage from './pages/PostListPage';
-import PostPage from './pages/PostPage';
-import WritePage from './pages/WritePage';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
 import { BrowserRouter } from 'react-router-dom';
 import Routing from './Routing';
 
