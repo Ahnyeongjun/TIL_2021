a1930629c911064da8cb2e5853c07ab445fdace8
jun <toinin@naver.com>
2021-03-28 15:07:27 +0900

    Delete api

--- react/react_study_book/24/big-frontend/src/Routing.js
+++ react/react_study_book/24/big-frontend/src/Routing.js
@@ -9,11 +9,14 @@ import RegisterPage from './pages/RegisterPage';
 const Routing = () => {
     return (
         <>
-            <Route component={PostListPage} path={['/@:username', '/']} exact />
-            <Route component={LoginPage} path="/login" />
-            <Route component={RegisterPage} path="/register" />
-            <Route component={WritePage} path="/write" />
-            <Route component={PostPage} path="/@:username/:postId" />
+            <Switch>
+                <Route component={PostListPage} path={['/@:username', '/']} exact />
+                <Route component={LoginPage} path="/login" />
+                <Route component={RegisterPage} path="/register" />
+                <Route component={WritePage} path="/write" />
+                <Route component={PostPage} path="/@:username/:postId" />
+            </Switch>
+
         </>
     )
 }
