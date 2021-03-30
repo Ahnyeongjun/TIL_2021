4ad4e7f35510189b46b0b924140626af21e5770b
jun <toinin@naver.com>
2021-03-30 23:46:13 +0900

    Fix writePage

--- react/react_study_book/24/big-frontend/src/pages/WritePage.js
+++ react/react_study_book/24/big-frontend/src/pages/WritePage.js
@@ -1,6 +1,13 @@
 import React from 'react';
-
+import Responsive from '../components/common/Responsive';
+import Editer from '../components/write/Editor';
+import TagBox from '../components/write/TagBox';
 const WritePage = () => {
-    return <div>글쓰기</div>
+    return (
+        <Responsive>
+            <Editer />
+            <TagBox />
+        </Responsive>
+    )
 }
 export default WritePage;
