491361c5b69f6af27c354c33f000ffc6fbe3913d
jun <toinin@naver.com>
2021-04-19 14:46:32 +0900

    Add study class - 04.19 - 5

---
+++ react/react_study_class/1/my-app/src/App.js
@@ -0,0 +1,6 @@
+import A from "./a";
+function App() {
+  return <A></A>;
+}
+
+export default App;
---
+++ react/react_study_class/1/my-app/src/a.js
@@ -0,0 +1,5 @@
+const a = () => {
+  const name = "a";
+  return <div>{name}</div>;
+};
+export default a;
