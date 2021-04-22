ab57fdd8be5fcfdafde5dc890c4ee3fbb937d135
jun <toinin@naver.com>
2021-04-22 19:20:47 +0900

    .

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express"); //express를 설치했기 때문에 가져올 수 있다.
+const app = express();
+
+app.get("/", (req, res) => {
+  console.log("AAAA");
+  res.send("Hello World!");
+});
+
+app.listen(5000);
