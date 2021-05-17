75cab5e9480cac227fedf81fae0a197c88cbc260
jun <toinin@naver.com>
2021-05-17 15:20:12 +0900

    Add react_study_class 배열 렌더링 (2021-05-17 6class)

--- react/react_study_class/1/my-app/src/App.js
+++ react/react_study_class/1/my-app/src/App.js
@@ -1,11 +1,62 @@
-import CounterContainer from "./components/CountContainer";
-import TodosContainer from "./components/TodosContainer";
 import InputSample from "./InputSample";
 import UserList from "./UserList";
+import { useRef, useState } from "react";
+import CreateUser from "./createUser";
 function App() {
+  const [inputs, setInput] = useState({
+    username: "",
+    email: "",
+  });
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      username: "name1",
+      email: "111",
+    },
+    {
+      id: 2,
+      username: "name2",
+      email: "222",
+    },
+    {
+      id: 3,
+      username: "name3",
+      email: "333",
+    },
+  ]);
+
+  const { username, email } = inputs;
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setInput({
+      ...inputs,
+      [name]: value,
+    });
+  };
+
+  const nextId = useRef(4);
+  const onCreate = () => {
+    const user = {
+      id: nextId.current,
+      username,
+      email,
+    };
+    setUsers([...users, user]);
+    setInput({
+      username: "",
+      email: "",
+    });
+    nextId.current += 1;
+  };
   return (
     <div>
-      <UserList />
+      <CreateUser
+        username={username}
+        email={email}
+        onCreate={onCreate}
+        onChange={onChange}
+      />
+      <UserList users={users} onCreate={onCreate} />
       <InputSample />
     </div>
   );
--- react/react_study_class/1/my-app/src/UserList.js
+++ react/react_study_class/1/my-app/src/UserList.js
@@ -5,29 +5,30 @@ function User({ user }) {
     </div>
   );
 }
-function UserList() {
-  const users = [
-    {
-      id: 1,
-      username: "name1",
-      email: "111",
-    },
-    {
-      id: 2,
-      username: "name2",
-      email: "222",
-    },
-    {
-      id: 1,
-      username: "name3",
-      email: "333",
-    },
-  ];
+function UserList({ users, onCreate }) {
+  //   const users = [
+  //     {
+  //       id: 1,
+  //       username: "name1",
+  //       email: "111",
+  //     },
+  //     {
+  //       id: 2,
+  //       username: "name2",
+  //       email: "222",
+  //     },
+  //     {
+  //       id: 3,
+  //       username: "name3",
+  //       email: "333",
+  //     },
+  //   ];
   return (
     <div>
       {users.map((user) => (
-        <User user={user} />
+        <User user={user} key={user.id} />
       ))}
+      <button onClick={onCreate}>click</button>
     </div>
   );
 }
