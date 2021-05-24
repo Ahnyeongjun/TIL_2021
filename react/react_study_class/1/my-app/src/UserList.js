02491b6309fb8a62f53935719818bf208e8a3b4b
jun <toinin@naver.com>
2021-05-24 14:38:03 +0900

    Add react_study_class 배열 렌더링 (2021-05-24 5class)

--- react/react_study_class/1/my-app/src/UserList.js
+++ react/react_study_class/1/my-app/src/UserList.js
@@ -1,35 +1,36 @@
-function User({ user }) {
+import React from "react";
+
+function User({ user, onRemove, onToggle }) {
   return (
     <div>
-      <b>{user.username}</b> <span>({user.email})</span>
+      <b
+        style={{
+          cursor: "pointer",
+          color: user.active ? "green" : "black",
+        }}
+        onClick={() => onToggle(user.id)}
+      >
+        {user.username}
+      </b>
+      <span>({user.email})</span>
+      <button onClick={() => onRemove(user.id)}>삭제</button>
     </div>
   );
 }
-function UserList({ users, onCreate }) {
-  //   const users = [
-  //     {
-  //       id: 1,
-  //       username: "name1",
-  //       email: "111",
-  //     },
-  //     {
-  //       id: 2,
-  //       username: "name2",
-  //       email: "222",
-  //     },
-  //     {
-  //       id: 3,
-  //       username: "name3",
-  //       email: "333",
-  //     },
-  //   ];
+
+function UserList({ users, onRemove, onToggle }) {
   return (
     <div>
       {users.map((user) => (
-        <User user={user} key={user.id} />
+        <User
+          user={user}
+          key={user.id}
+          onRemove={onRemove}
+          onToggle={onToggle}
+        />
       ))}
-      <button onClick={onCreate}>click</button>
     </div>
   );
 }
+
 export default UserList;
