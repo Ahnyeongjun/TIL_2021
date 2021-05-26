706e7cff379c29102299db669e84f560ce8f78a1
jun <toinin@naver.com>
2021-05-26 15:09:59 +0900

    Add react_study_class 배열 렌더링 (2021-05-26 6 class)

--- react/react_study_class/1/my-app/src/UserList.js
+++ react/react_study_class/1/my-app/src/UserList.js
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 function User({ user, onRemove, onToggle }) {
+  const { username, email, id, active } = user;
+  useEffect(() => {
+    console.log("컴포넌트가 화면에 나타남");
+    console.log(user);
+    return () => {
+      console.log("컴포넌트과 화면에 사라짐");
+    };
+  }, [user]);
+
   return (
     <div>
       <b
